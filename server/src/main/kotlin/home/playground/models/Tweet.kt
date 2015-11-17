5990b6de3ba1e4efe5bf8e09ba255b8154560a87
Cuong Cu <csc823@gmail.com>
2015-11-17 01:35:04 -0600

    Separate server and client code

--- server/src/main/kotlin/home/playground/models/Tweet.kt
+++ server/src/main/kotlin/home/playground/models/Tweet.kt
@@ -7,13 +7,32 @@ import java.util.*
 /**
  * Created by cuong on 11/14/15.
  */
+fun resolveAgeGroup(age: Int): String
+{
+    if (age < 18) {
+            return "child"
+    } else if (age <= 24) {
+            return "adult18to24"
+    } else if (age <= 34) {
+            return "adult25to34"
+    } else if (age <= 44) {
+            return "adult35to44"
+    } else if (age <= 54) {
+            return "adult45to54"
+    } else if (age <= 64) {
+            return "adult55to64"
+    } else {
+            return "adultOver64"
+    }
+}
+
 data class Tweet (
-        val text           : String    = "",
-        val profileImageUrl: String    = "",
-        val hashTag        : String    = "",
-        val createdDate    : Timestamp = Timestamp(Date().time),
-        var gender         : String?   = null, // Enum: Male, Female, Unknown
-        var sentiment      : String    = "",   // Enum: Pos, Neg, Neu, Unknown
-        var age            : Int?      = null,
-        val mediaType      : String    = ""
+    val text           : String    = "",
+    val profileImageUrl: String    = "",
+    val hashTag        : String    = "",
+    val createdDate    : Timestamp = Timestamp(Date().time),
+    var gender         : String?   = null, // Enum: Male, Female, Unknown
+    var sentiment      : String    = "",   // Enum: Pos, Neg, Neu, Unknown
+    var age            : Int?      = null,
+    val mediaType      : String    = ""
 ) : Serializable
--- server/src/main/kotlin/home/playground/resources/TweetResource.kt
+++ server/src/main/kotlin/home/playground/resources/TweetResource.kt
@@ -65,22 +65,21 @@ class TweetResource
 
         return tweetRdd!!.filter { it.hashTag == hashTag }
             .groupBy { it.sentiment }
-            .collectAsMap()
             .mapValues {
-                it.value.groupBy { resolveAgeGroup(it.age as Int) }
-                        .mapValues {
-                            it.value.partition { it.gender == "male" }
-                                    .let { mapOf("male" to it.first.size, "female" to it.second.size) }
-                        }
+                it.groupBy { resolveAgeGroup(it.age as Int) }
+                    .mapValues {
+                        it.value.partition { it.gender == "male" }
+                                .let { mapOf("male" to it.first.size, "female" to it.second.size) }
+                    }
             }
             .map {
                 SentimentStatistic(
-                    freq = it.value,
-                    sentiment = it.key,
-                    total = it.value.values.fold(0) { total, ageGroup -> total + ageGroup.values.sum() }
+                    freq = it._2,
+                    sentiment = it._1,
+                    total = it._2.values.fold(0) { total, ageGroup -> total + ageGroup.values.sum() }
                 )
             }
-            .toList()
+            .collect()
     }
 
     @ResponseBody
@@ -91,22 +90,5 @@ class TweetResource
         return fas.faceAnalysis(imgUrl)
     }
 
-    private fun resolveAgeGroup(age: Int): String
-    {
-        if (age < 18) {
-            return "child"
-        } else if (age <= 24) {
-            return "adult18to24"
-        } else if (age <= 34) {
-            return "adult25to34"
-        } else if (age <= 44) {
-            return "adult35to44"
-        } else if (age <= 54) {
-            return "adult45to54"
-        } else if (age <= 64) {
-            return "adult55to64"
-        } else {
-            return "adultOver64"
-        }
-    }
+
 }
